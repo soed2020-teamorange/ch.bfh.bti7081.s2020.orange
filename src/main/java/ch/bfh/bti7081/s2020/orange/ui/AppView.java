2676d19ea98ef3073548a6ace2e9cd9ed6757338
Fabian Küng <fabian.kueng@inpeek.ch>
2020-05-23 14:33:32 +0200

    feat: add emergency button to bottom instead of nav

---
+++ src/main/java/ch/bfh/bti7081/s2020/orange/ui/AppView.java
@@ -0,0 +1,57 @@
+package ch.bfh.bti7081.s2020.orange.ui;
+
+import static ch.bfh.bti7081.s2020.orange.ui.utils.AppConst.VIEWPORT;
+
+import ch.bfh.bti7081.s2020.orange.application.security.CurrentUser;
+import ch.bfh.bti7081.s2020.orange.backend.data.Role;
+import ch.bfh.bti7081.s2020.orange.backend.data.entities.MedicalSpecialist;
+import ch.bfh.bti7081.s2020.orange.backend.data.entities.Patient;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.contextmenu.ContextMenu;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.component.page.Viewport;
+import com.vaadin.flow.router.RouterLayout;
+import com.vaadin.flow.server.PWA;
+import javax.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+
+@Push
+@Viewport(VIEWPORT)
+@PWA(name = "Team Orange - Projekt MHC-PMS", shortName = "MHC-PMS Orange", startPath = "login")
+@RequiredArgsConstructor
+@CssImport("./styles/shared-styles.css")
+public class AppView extends Div implements RouterLayout {
+
+  private final CurrentUser currentUser;
+
+  @PostConstruct
+  public void init() {
+    setWidth("100%");
+    setHeight("100%");
+
+    if (currentUser.getUser().getRole().equals(Role.PATIENT)) {
+      final Button emergencyButton = new Button(new Icon(VaadinIcon.PHONE));
+      emergencyButton.addClassName("emergency-button");
+      ContextMenu contextMenu = new ContextMenu(emergencyButton);
+      contextMenu.addItem(new Anchor("tel://144", "Notruf: 144"));
+      contextMenu.addItem(new Anchor("tel://143", "Dargebotene Hand: 143"));
+
+      Patient patient = (Patient) currentUser.getUser();
+      MedicalSpecialist medicalSpecialist = patient.getMedicalSpecialist();
+      if (medicalSpecialist.getPhone() != null) {
+        contextMenu.addItem(
+            medicalSpecialist.getFirstName() + " " + medicalSpecialist
+                .getLastName() + ": " + medicalSpecialist.getPhone());
+      }
+      contextMenu.setOpenOnClick(true);
+      contextMenu.setEnabled(false);
+
+      add(emergencyButton);
+    }
+  }
+}
